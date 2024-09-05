ae4174ca62170aee400aeccb895678aa02593aa1
thz <2745530161@qq.com>
2024-09-05 22:44:31 +0800

    [Improve]Supports clearing tables before Dameng database sink

--- seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/dm/DamengCatalog.java
+++ seatunnel-connectors-v2/connector-jdbc/src/main/java/org/apache/seatunnel/connectors/seatunnel/jdbc/catalog/dm/DamengCatalog.java
@@ -94,6 +94,12 @@ public class DamengCatalog extends AbstractJdbcCatalog {
     }
 
     @Override
+    protected String getTruncateTableSql(TablePath tablePath) throws CatalogException {
+        return String.format(
+                "TRUNCATE TABLE `%s`.`%s`;", tablePath.getSchemaName(), tablePath.getTableName());
+    }
+
+    @Override
     protected String getTableName(TablePath tablePath) {
         return tablePath.getSchemaAndTableName().toUpperCase();
     }
