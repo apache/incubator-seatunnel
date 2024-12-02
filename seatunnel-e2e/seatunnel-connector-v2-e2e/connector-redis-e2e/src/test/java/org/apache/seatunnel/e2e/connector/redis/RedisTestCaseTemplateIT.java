a1ea524bcdc8e8cac96a3f64fdf5f45a891358b8
limin <limin@wedobest.com.cn>
2024-12-02 11:52:19 +0800

    add some e2e test cases

--- seatunnel-e2e/seatunnel-connector-v2-e2e/connector-redis-e2e/src/test/java/org/apache/seatunnel/e2e/connector/redis/RedisTestCaseTemplateIT.java
+++ seatunnel-e2e/seatunnel-connector-v2-e2e/connector-redis-e2e/src/test/java/org/apache/seatunnel/e2e/connector/redis/RedisTestCaseTemplateIT.java
@@ -57,6 +57,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Stream;
 
 @Slf4j
@@ -499,6 +500,82 @@ public abstract class RedisTestCaseTemplateIT extends TestSuiteBase implements T
         Assertions.assertEquals(0, execResult.getExitCode());
         Assertions.assertEquals(2, jedis.zcard("zset_check"));
         jedis.del("zset_check");
+    }
+
+    @TestTemplate
+    public void testFakeToRedisNormalKeyIsNullTest(TestContainer container)
+            throws IOException, InterruptedException {
+        Container.ExecResult execResult =
+                container.executeJob("/fake-to-redis-test-normal-key-is-null.conf");
+        Assertions.assertEquals(0, execResult.getExitCode());
+        int count = 0;
+        String data = jedis.get("");
+        if (data != null) {
+            count++;
+            jedis.del("");
+        }
+        for (int i = 2; i <= 3; i++) {
+            data = jedis.get("NEW" + i);
+            if (data != null) {
+                count++;
+                jedis.del("NEW" + i);
+            }
+        }
+        Assertions.assertEquals(2, count);
+    }
+
+    @TestTemplate
+    public void testFakeToRedisCustomKeyIsNullTest(TestContainer container)
+            throws IOException, InterruptedException {
+        Container.ExecResult execResult =
+                container.executeJob("/fake-to-redis-test-custom-key-is-null.conf");
+        Assertions.assertEquals(0, execResult.getExitCode());
+        int count = 0;
+        String data = jedis.get("key_check:");
+        if (data != null) {
+            count++;
+            jedis.del("key_check:");
+        }
+        for (int i = 2; i <= 3; i++) {
+            data = jedis.get("key_check:NEW" + i);
+            if (data != null) {
+                count++;
+                jedis.del("key_check:NEW" + i);
+            }
+        }
+        Assertions.assertEquals(2, count);
+    }
+
+    @TestTemplate
+    public void testFakeToRedisOtherTypeValueIsNullTest(TestContainer container)
+            throws IOException, InterruptedException {
+        Container.ExecResult execResult =
+                container.executeJob(
+                        "/fake-to-redis-test-custom-value-when-other-type-is-null.conf");
+        Assertions.assertEquals(0, execResult.getExitCode());
+        Assertions.assertEquals(2, jedis.llen("list_check"));
+        jedis.del("list_check");
+    }
+
+    @TestTemplate
+    public void testFakeToRedisHashTypeKeyIsNullTest(TestContainer container)
+            throws IOException, InterruptedException {
+        Container.ExecResult execResult =
+                container.executeJob("/fake-to-redis-test-custom-value-when-hash-key-is-null.conf");
+        Assertions.assertEquals(0, execResult.getExitCode());
+        Assertions.assertEquals(2, jedis.hlen("hash_check"));
+        jedis.del("hash_check");
+    }
+
+    @TestTemplate
+    public void testFakeToRedisHashTypeValueIsNullTest(TestContainer container)
+            throws IOException, InterruptedException {
+        Container.ExecResult execResult =
+                container.executeJob(
+                        "/fake-to-redis-test-custom-value-when-hash-value-is-null.conf");
+        Assertions.assertEquals(0, execResult.getExitCode());
+        Assertions.assertEquals(2, jedis.hlen("hash_check"));
+        jedis.del("hash_check");
     }
 
     public abstract RedisContainerInfo getRedisContainerInfo();
