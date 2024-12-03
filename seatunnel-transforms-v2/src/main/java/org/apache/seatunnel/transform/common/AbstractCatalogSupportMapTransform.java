9bdbc3a5da93ead88497907e8d17bbdb55fd627c
njh_cmss <nijiahui_yewu@cmss.chinamobile.com>
2024-12-03 16:26:29 +0800

    [Feature][transform-v2] Support transform metrics

--- seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/common/AbstractCatalogSupportMapTransform.java
+++ seatunnel-transforms-v2/src/main/java/org/apache/seatunnel/transform/common/AbstractCatalogSupportMapTransform.java
@@ -25,6 +25,8 @@ import org.apache.seatunnel.api.transform.SeaTunnelMapTransform;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Objects;
+
 @Slf4j
 public abstract class AbstractCatalogSupportMapTransform
         extends AbstractSeaTunnelTransform<SeaTunnelRow, SeaTunnelRow>
@@ -44,7 +46,10 @@ public abstract class AbstractCatalogSupportMapTransform
     @Override
     public SeaTunnelRow map(SeaTunnelRow row) {
         SeaTunnelRow result = transform(row);
-        hazelcastMetric();
+        if (Objects.nonNull(result)) {
+            hazelcastMetric();
+        }
+
         return result;
     }
 }
