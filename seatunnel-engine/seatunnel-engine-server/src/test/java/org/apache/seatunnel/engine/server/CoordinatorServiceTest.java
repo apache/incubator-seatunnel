f501c5a607a92cb869e54dfc19395729781730a2
ClownXC <chen3494269@163.com>
2024-12-09 00:57:34 +0800

    add new tests

--- seatunnel-engine/seatunnel-engine-server/src/test/java/org/apache/seatunnel/engine/server/CoordinatorServiceTest.java
+++ seatunnel-engine/seatunnel-engine-server/src/test/java/org/apache/seatunnel/engine/server/CoordinatorServiceTest.java
@@ -138,10 +138,6 @@ public class CoordinatorServiceTest {
         // because runningJobMasterMap is empty and we have no JobHistoryServer, so return
         // UNKNOWABLE.
         Assertions.assertTrue(JobStatus.UNKNOWABLE.equals(coordinatorService.getJobStatus(jobId)));
-        Assertions.assertEquals(
-                30, coordinatorService.getThreadPoolStatusMetrics().getCorePoolSize());
-        Assertions.assertEquals(
-                1000, coordinatorService.getThreadPoolStatusMetrics().getMaximumPoolSize());
         coordinatorServiceTest.shutdown();
     }
 
