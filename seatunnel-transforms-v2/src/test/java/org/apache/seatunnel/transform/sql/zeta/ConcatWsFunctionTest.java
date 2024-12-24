9f2afe5950566429f4fb4a26eade9183594ea75d
binwang219962 <binwang219962@sohu-inc.com>
2024-12-24 19:57:12 +0800

    [Hotfix][Transform][Sql] function concat_ws handle array type， avoid write [Ljava.lang.String.

--- seatunnel-transforms-v2/src/test/java/org/apache/seatunnel/transform/sql/zeta/ConcatWsFunctionTest.java
+++ seatunnel-transforms-v2/src/test/java/org/apache/seatunnel/transform/sql/zeta/ConcatWsFunctionTest.java
@@ -6,42 +6,45 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ConcatWsFunctionTest {
 
     @Test
     public void testConcatWs() {
-        Assertions.assertEquals("a;b", StringFunction.concatWs(genArgs1()));
-        Assertions.assertEquals("a;b;c", StringFunction.concatWs(genArgs2()));
-        Assertions.assertEquals("", StringFunction.concatWs(genArgs3()));
+        Assertions.assertEquals("", StringFunction.concatWs(genArgs(";", new String[] {})));
+        Assertions.assertEquals("", StringFunction.concatWs(genArgs(null, new String[] {})));
+        Assertions.assertEquals(
+                "a;b", StringFunction.concatWs(genArgs(";", new String[] {"a", "b"})));
+        Assertions.assertEquals(
+                "a;b", StringFunction.concatWs(genArgs(";", new String[] {"a", null, "b"})));
+        Assertions.assertEquals(
+                "ab",
+                StringFunction.concatWs(genArgs("", new String[] {null, "a", null, "b", null})));
+        Assertions.assertEquals(
+                "ab", StringFunction.concatWs(genArgs(null, new String[] {"a", "b", null})));
+        Assertions.assertEquals(
+                "a;b;c", StringFunction.concatWs(genArgs(";", new String[] {"a", "b"}, "c")));
+        Assertions.assertEquals(
+                "a;b", StringFunction.concatWs(genArgs(";", new String[] {"a", "b"}, null)));
+        Assertions.assertEquals(
+                "a;b;1;2",
+                StringFunction.concatWs(
+                        genArgs(";", new String[] {"a", "b"}, new String[] {"1", "2"})));
     }
 
-    public List<Object> genArgs1() {
+    public List<Object> genArgs(String separator, String[] arr) {
         List<Object> list = new ArrayList<>();
-        String[] arr = new String[] {"a", "b"};
-        String separator = ";";
         list.add(separator);
         list.add(arr);
         return list;
     }
 
-    public List<Object> genArgs2() {
+    public List<Object> genArgs(String separator, Object... arr) {
         List<Object> list = new ArrayList<>();
-        String[] arr = new String[] {"a", "b"};
-        String separator = ";";
         list.add(separator);
-        list.add(arr);
-        list.add("c");
-        return list;
-    }
-
-    public List<Object> genArgs3() {
-        List<Object> list = new ArrayList<>();
-        String[] arr = new String[] {};
-        String separator = ";";
-        list.add(separator);
-        list.add(arr);
+        Collections.addAll(list, arr);
         return list;
     }
 }
